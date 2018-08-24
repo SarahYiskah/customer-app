698ab8bcd4bc244a32ea47f84693c4c1a952eb2f
SarahYiskah <sarahy97@gmail.com>
2018-08-24 14:25:39 -0400

    error validations

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const expressValidator = require('express-validator')
 
 const app = express()
 
@@ -14,6 +15,15 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 //set static path
 app.use(express.static(path.join(__dirname, 'public')))
+
+//Global vars
+app.use((req, res, next) => {
+  res.locals.errors = null
+  next()
+})
+
+// express validator middleware
+app.use(expressValidator());﻿
 
 let users = [
   {
@@ -36,11 +46,34 @@ let users = [
   }
 ]
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.render('index', {
     title: 'Cutomers',
     users: users
    });
+})
+
+app.post('/users/add', (req, res) => {
+  req.checkBody('first_name', 'First name is required').notEmpty()
+  req.checkBody('last_name', 'Last name is required').notEmpty()
+  req.checkBody('email', 'Email is required').notEmpty()
+
+  let errors = req.validationErrors()
+
+  if(errors) {
+    res.render('index', {
+      title: 'Cutomers',
+      users: users,
+      errors: errors
+     });
+  } else {
+    let newUser = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email
+    }
+    console.log(newUser)
+  }
 })
 
 app.listen(3000, () => console.log('server started on port 3000'))
